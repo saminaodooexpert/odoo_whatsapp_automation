b4898bb98867e342bd86c0096b84cc9fd79302ff
Samina Moin <saminamoinit@gmail.com>
2026-04-09 16:04:48 +0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'WhatsApp Meta Automation',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Automated WhatsApp Messages for Odoo 18',
+    'author': 'Samina Moin',
+    'depends': ['sale', 'base'],
+    'data': [
+        'views/whatsapp_button_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
